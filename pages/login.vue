075c503513ca367baae36d0a599d109612876709
Sander <thesannio123@gmail.com>
2022-12-09 16:09:47 +0100

    updated login page design
    also added an svg image to the new assets folder

--- pages/login.vue
+++ pages/login.vue
@@ -1,21 +1,23 @@
 <template>
     <main>
-        <section class="loginWindow">
-            <h1>LeerLevels CMS<br>management portal login</h1>
-                <section class="loginRow">
-                    <label class="textlabels">email address</label>
-                    <section class="inputSection">
-                        <input class="loginInput" type="text" v-model="emailInput"/>
-                    </section>
+        <section class="login-window">
+            <img class="LeerLevels-image" src="@/assets/LeerLevels_Logo_Horizontal.svg"/>
+            <h1>CMS Management portal login</h1>
+                <section class="pure-form pure-form-stacked">
+                    <fieldset>
+                        <section class="pure-control-group">
+                            <label for="stacked-email" class="labelText">Email Address</label>
+                            <input type="email" id="stacked-email" placeholder="Email Address" required="" v-model="emailInput"/>
+                        </section>
+                        <section class="pure-control-group">
+                            <label for="stacked-password" class="labelText">Password</label>
+                            <input type="password" id="stacked-password" placeholder="Password" required="" v-model="passwordInput"/>
+                        </section>
+                        <section class="pure-controls">
+                            <button class="pure-button pure-button-primary" @click="Login()">Login</button>
+                        </section>
+                    </fieldset>
                 </section>
-
-                <section class="loginRow">
-                    <label class="textlabels">password</label>
-                    <section class="inputSection">
-                        <input class="loginInput" type="password" v-model="passwordInput"/>
-                    </section>
-                </section>
-                <button @click="Login()" :disabled="!enableLogin">Login</button>
         </section>
     </main>
 </template>
@@ -25,10 +27,9 @@ export default {
     name: "login",
     data() {
         return {
-            enableLogin: true,
             emailInput: "",
             passwordInput: "",
-            regex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
+            emailRegex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
         }
     },
     methods: {
@@ -43,37 +44,59 @@ export default {
                     }
                 }
             } else {
-                // setting this to false would permanently disable the login button.
-                // enableLogin = false;
+
                 console.log("the email address could not be validated");
             }
 
         },
         checkEmail() {
             //this will return true if the input is not empty and matches the regular expression of a valid email address, otherwise it returns false
-            return !!((this.emailInput === "") ? "" : !!(this.regex.test(this.emailInput)));
+            return !!((this.emailInput === "") ? "" : !!(this.emailRegex.test(this.emailInput)));
         },
     }
 }
 </script>
 
 <style scoped>
-.loginWindow {
-    margin: 40px 400px;
-}
-.loginRow{
-    margin: 20px 0;
-}
-.inputSection {
-    margin: 10px 0;
-}
-.loginInput{
-    height: 1.6em;
+
+.body {
+    background-color: gray;
 }
 
-.textlabels{
-    font-size: 1.5em;
-    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    text-align: left;
+main {
+    max-width: none;
+    padding: 0.25em 0.5em 22.4em 0.5em;
+    background-color: gray;
 }
+
+.login-window {
+    margin: 40px auto;
+    width: 700px;
+    padding: 3em;
+    background-color: white;
+    border-radius: 25px;
+}
+
+.LeerLevels-image {
+    width: 455px;
+}
+
+h1 {
+    font-size: 2em;
+    margin: 0.5em 0;
+    font-family: revert;
+}
+
+.pure-form, .pure-control-group, .pure-controls {
+    margin: 1em 0;
+}
+
+.labelText {
+    margin-bottom: 0.85em;
+}
+
+#stacked-email, #stacked-password {
+    width: 57%;
+}
+
 </style>
