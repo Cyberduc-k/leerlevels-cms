f51f9b6cd60c5f1d061443d93188bd841d9d7514
Cyberduc-k <tttymo@gmail.com>
2022-12-10 13:25:14 +0100

    turn login page into form

--- pages/login.vue
+++ pages/login.vue
@@ -3,37 +3,42 @@
         <section class="login-window">
             <img class="LeerLevels-image" src="@/assets/LeerLevels_Logo_Horizontal.svg"/>
             <h1>CMS Management portal login</h1>
-                <section class="pure-form pure-form-stacked">
+                <form class="pure-form pure-form-stacked" @submit="login">
                     <fieldset>
                         <section class="pure-control-group">
                             <label for="stacked-email" class="labelText">Email Address</label>
-                            <input type="email" id="stacked-email" placeholder="Email Address" required="" v-model="emailInput"/>
+                            <input type="email" id="stacked-email" placeholder="Email Address" required v-model="emailInput" @input="validate"/>
                         </section>
                         <section class="pure-control-group">
                             <label for="stacked-password" class="labelText">Password</label>
-                            <input type="password" id="stacked-password" placeholder="Password" required="" v-model="passwordInput"/>
+                            <input type="password" id="stacked-password" placeholder="Password" required v-model="passwordInput" @input="validate" @keypress.prevent.enter="login"/>
                         </section>
                         <section class="pure-controls">
-                            <button class="pure-button pure-button-primary" @click="Login()">Login</button>
+                            <input type="submit" class="pure-button pure-button-primary" :disabled="!isValid" value="Login" />
                         </section>
                     </fieldset>
-                </section>
+                </form>
         </section>
     </main>
 </template>
 
 <script>
-export default {
-    name: "login",
+import { defineComponent } from "vue"
+
+export default defineComponent({
     data() {
         return {
+            isValid: false,
             emailInput: "",
             passwordInput: "",
             emailRegex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
         }
     },
     methods: {
-        async Login() {
+        validate() {
+            this.isValid = this.checkEmail() && this.passwordInput !== "";
+        },
+        async login() {
             if (this.checkEmail()) {
                 if (await this.$store.dispatch("login", { email: this.emailInput, password: this.passwordInput })) {
                     this.emailInput = "";
@@ -41,31 +46,31 @@ export default {
                     
                     if (this.$route.query.next) {
                         this.$router.push(this.$route.query.next);
+                    } else {
+                        this.$router.push("/");
                     }
                 }
             } else {
-
                 console.log("the email address could not be validated");
             }
-
         },
         checkEmail() {
             //this will return true if the input is not empty and matches the regular expression of a valid email address, otherwise it returns false
             return !!((this.emailInput === "") ? "" : !!(this.emailRegex.test(this.emailInput)));
         },
     }
-}
+});
 </script>
 
 <style scoped>
-
 .body {
     background-color: gray;
 }
 
 main {
+    min-height: calc(100vh - 0.5em);
     max-width: none;
-    padding: 0.25em 0.5em 22.4em 0.5em;
+    padding: 0.25em 0.5em 0.25em 0.5em;
     background-color: gray;
 }
 
@@ -98,5 +103,4 @@ h1 {
 #stacked-email, #stacked-password {
     width: 57%;
 }
-
 </style>
